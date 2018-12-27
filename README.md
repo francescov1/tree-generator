f34f2c251f17d755ee7db0e7aaaab13e7d664456
Francesco Virga <francescod.virga@gmail.com>
2018-12-26 20:48:09 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Tree Generator
+
+This script generates a markdown representation of your current directory structure, resembeling the following:
+
+├── index.js
+├── tests
+│ ├── moreTests
+│ │ ├── test3.js
+│ ├── test1.js
+│ ├── test2.js
+├── controllers
+│ ├── controller1.js
+├── models
+│ ├── model1.js
+└─└── model2.js
+
+The structure is saved in `tree.md`.
---
+++ tree-generator.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# TODO: pass number of levels deep to go
+
+explore_dir() {
+  level=$1
+
+  for item in * .[^.]*; do
+    if [[ $item != "*" && $item != ".[^.]*" ]]; then
+      str=""
+      if [[ $level != 0 ]]; then
+        for i in $(seq 1 $level); do str+='│   '; done
+      fi
+
+      str="${str}├── ${item}"
+      echo $str >> ${root}/tree.md
+
+      if [[ -d $item ]]; then
+        (cd $item && explore_dir $(($level + 1)))
+      fi
+    fi
+
+  done
+}
+
+if [ -f tree.md ]; then
+   rm tree.md
+fi
+
+root=$(pwd)
+explore_dir 0
+
+# this part replaces the end with a closing branch (├── -> └──)
+# TODO: this could be done cleaner
+last_line=$(grep "." tree.md | tail -1)
+last_line=$(echo "$last_line" | tr ├ └)
+last_line="└─${last_line:2}"
+sed -i '' -e '$ d' tree.md
+echo $last_line >> ${root}/tree.md
+
+echo "The following directory tree can be found in tree.md:"
+cat tree.md
